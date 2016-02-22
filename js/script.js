654d9d789e661d79f3af0f377628a8816dafd128
cranderveldt <djnaphouse@gmail.com>
2016-02-22 16:38:24 -0500

    click to toggle

--- js/script.js
+++ js/script.js
@@ -1,15 +1,10 @@
 var app = angular.module('witness', []);
 app.controller('Main',['$scope', function ($scope) {
-  $scope.shapes = [];
   $scope.points = [];
 
   for (var i = 0; i < 81; i++) {
-    $scope.points.push({ sides: { north: false, south: false, east: false, west: false } });
+    $scope.points.push({ active: false, sides: { north: false, south: false, east: false, west: false } });
   }
-
-  $scope.addShape = function() {
-    $scope.shapes.push({ position: { top: 0, left: 0 } });
-  };
 
   $scope.getPointClasses = function(point) {
     var klasses = [];
@@ -30,37 +25,17 @@ app.controller('Main',['$scope', function ($scope) {
       klasses.push("west-active");
     }
 
+    if (point.active) {
+      klasses.push("main-active");
+    }
+
     return klasses.join(' ');
   };
 
   $scope.digestPoint = function(point) {
-    console.log(point.sides);
     $scope.$digest();
   };
 }]);
-
-app.directive('witShape', function () {
-  var linkFunction = function ($scope, element, attrs) {
-    var draggable_options = {
-      grid: [100, 100]
-      , stop: function(e, ui) {
-        $scope.witShape.position = { top: ui.position.top, left: ui.position.left };
-      }
-    };
-    
-    element.draggable(draggable_options);
-  };
-  var directiveDefinitionObject = {
-    replace: false,
-    transclude: false,
-    restrict: 'A',
-    scope: {
-      witShape: '='
-    },
-    link: linkFunction
-  };
-  return directiveDefinitionObject;
-});
 
 app.directive('witPoint', function () {
   var linkFunction = function ($scope, element, attrs) {
@@ -76,9 +51,11 @@ app.directive('witPoint', function () {
       if (offset.top <= 10) {
         $scope.witPoint.sides.north = !$scope.witPoint.sides.north;
       }
-
       if (offset.left <= 10) {
         $scope.witPoint.sides.west = !$scope.witPoint.sides.west;
+      }
+      if (offset.left > 10 && offset.top > 10) {
+        $scope.witPoint.active = !$scope.witPoint.active;
       }
 
       $scope.witCallback($scope.witPoint);
